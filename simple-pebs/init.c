66c681493d4b0bf3c5e7f672d7ac4d615e0e1f95
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-02-15 17:13:43 +0900

    specify counter by name

--- simple-pebs/init.c
+++ simple-pebs/init.c
@@ -5,12 +5,16 @@
 #include <sys/ioctl.h>
 #include "simple-pebs.h"
 
+const unsigned int UOPS_RETIRED_ALL = 0x1C2;
+const unsigned int MEM_LOAD_UOPS_RETIRED_L3_MISS = 0x20D1;
+const unsigned int page_size = 4096;
+
 int main(){
   int fd, ret;
   struct simple_pebs_parameter param = {
-    .pebs_event = 0x20D1,
-    .reset_value = 0x64,
-    .buffer_size = 64 * 1024
+    .pebs_event = MEM_LOAD_UOPS_RETIRED_L3_MISS,
+    .reset_value = 100,
+    .buffer_size = (page_size << 8),
     .output_mode = 0,
   };
   
